14392af34d0aad1bab80491b26095030902c3aa6
Arif <md.arifuz.zaman@g.bracu.ac.bd>
2025-09-10 15:24:45 +0600

    App is running few error needs to be resolved

--- main.py
+++ main.py
@@ -18,6 +18,48 @@ def main():
 
     st.markdown(html_temp, unsafe_allow_html=True)
 
+    model = joblib.load("insurance_model")
+    st.sidebar.title("Navigation")
+    p1 = st.slider('Enter your age', 0, 18, 100)
+    s1 = st.selectbox('Sex',('Male', 'Female', 'Other'))
+    if s1=='Male':
+        p2=1
+    else:
+        p2=0
+    
+    p3 = st.number_input('Enter your BMI', 0.0, 100.0, 25.0)
+    p4 = st.number_input('Enter your number of children', 0, 20, 0)
+    s2 = st.selectbox('Do you smoke?',('Yes', 'No'))
+    if s2=='Yes':
+        p5=1
+    else:
+        p5=0
+    s3 = st.selectbox('Region',('Northeast', 'Northwest', 'Southeast', 'Southwest'))
+    if s3=='Northeast':
+        p6=0
+        p7=0
+        p8=0
+    elif s3=='Northwest':
+        p6=1
+        p7=0
+        p8=0
+    elif s3=='Southeast':
+        p6=0
+        p7=1
+        p8=0
+    else:
+        p6=0
+        p7=0
+        p8=1        
+    if st.button("Predict"):
+        prediction = model.predict([[p1, p2, p3, p4, p5, p6, p7, p8]])
+        st.success(f'Your insurance cost is $ {prediction[0]:.2f}') 
+    if st.button("About"):
+        st.text("Built with Streamlit")
+        st.text("Author: blackcat")
+    
+
+
 
 if __name__ == "__main__":
     main()
